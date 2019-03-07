ed2868ef83a3555d287b6379aa0c701db68ed73b
Mohsen Davoodi <mohsen.31369@gmail.com>
2019-03-07 17:42:05 +0330

    add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,104 @@
+import nodeResolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import replace from 'rollup-plugin-replace'
+import { terser } from 'rollup-plugin-terser'
+
+import pkg from './package.json'
+
+export default [
+  // CommonJS
+  {
+    input: 'src/index.js',
+    output: { file: 'build/cjs/hoverintent.js', format: 'cjs', indent: false },
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {})
+    ],
+    plugins: [babel()]
+  },
+
+  // ES
+  {
+    input: 'src/index.js',
+    output: { file: 'build/es/hoverintent.js', format: 'es', indent: false },
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {})
+    ],
+    plugins: [babel()]
+  },
+
+  // ES for Browsers
+  {
+    input: 'src/index.js',
+    output: { file: 'build/es/hoverintent.mjs', format: 'es', indent: false },
+    plugins: [
+      nodeResolve({
+        jsnext: true
+      }),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+      terser({
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          warnings: false
+        }
+      })
+    ]
+  },
+
+  // UMD Development
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'build/umd/hoverintent.js',
+      format: 'umd',
+      name: 'HoverIntent',
+      indent: false
+    },
+    plugins: [
+      nodeResolve({
+        jsnext: true
+      }),
+      babel({
+        exclude: 'node_modules/**'
+      }),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('development')
+      })
+    ]
+  },
+
+  // UMD Production
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'build/umd/hoverintent.min.js',
+      format: 'umd',
+      name: 'hoverintent',
+      indent: false
+    },
+    plugins: [
+      nodeResolve({
+        jsnext: true
+      }),
+      babel({
+        exclude: 'node_modules/**'
+      }),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+      terser({
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          warnings: false
+        }
+      })
+    ]
+  }
+]
