fa5479e4b1040a496148f0ec44fca9b6b40b7fa8
Mohsen Davoodi <mohsen.31369@gmail.com>
2019-03-10 13:20:32 +0330

    add constant type

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import debounce from 'lodash-es/debounce'
 
+const TYPE_ENTER = 'enter'
+const TYPE_OUT = 'out'
+
 const intent = (type, element, func, wait = 0) => {
   if (typeof func !== 'function') {
     throw new TypeError('Expected a function')
@@ -7,20 +10,20 @@ const intent = (type, element, func, wait = 0) => {
   const debouncer = debounce(func, +wait)
   const cancelDebounce = debouncer.cancel
   const cancel = () => {
-    element.removeEventListener(type === 'enter' ? 'mouseenter' : 'mouseout', debouncer)
-    element.removeEventListener(type === 'enter' ? 'mouseout' : 'mouseenter', cancelDebounce)
+    element.removeEventListener(type === TYPE_ENTER ? 'mouseenter' : 'mouseout', debouncer)
+    element.removeEventListener(type === TYPE_ENTER ? 'mouseout' : 'mouseenter', cancelDebounce)
     cancelDebounce()
   }
-  element.addEventListener(type === 'enter' ? 'mouseenter' : 'mouseout', debouncer)
-  element.addEventListener(type === 'enter' ? 'mouseout' : 'mouseenter', cancelDebounce)
+  element.addEventListener(type === TYPE_ENTER ? 'mouseenter' : 'mouseout', debouncer)
+  element.addEventListener(type === TYPE_ENTER ? 'mouseout' : 'mouseenter', cancelDebounce)
 
   return {
     cancel
   }
 }
 
-const enter = (...args) => intent('enter', ...args)
+const enter = (...args) => intent(TYPE_ENTER, ...args)
 
-const out = (...args) => intent('out', ...args)
+const out = (...args) => intent(TYPE_OUT, ...args)
 
 export { enter, out }
