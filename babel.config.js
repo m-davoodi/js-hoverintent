118c4d0d3e9b91f242742f6c8491a1ee1e5702ac
Mohsen Davoodi <mohsen.31369@gmail.com>
2019-03-12 18:42:23 +0330

    rename .babelrc.js

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: false
+      }
+    ]
+  ],
+  env: {
+    test: {
+      plugins: ['@babel/plugin-transform-modules-commonjs']
+    }
+  }
+}
