3e05677ed78fa7af550740fedad1531d6001c0a7
Mohsen Davoodi <mohsen.31369@gmail.com>
2019-04-09 13:11:59 +0430

    remove debounce module and add it into index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-import debounce from './debounce'
+const debounce = (callback, wait) => {
+  let timeout
+  const cancel = () => {
+    clearTimeout(timeout)
+  }
+  const debounced = (...args) => {
+    cancel()
+    timeout = setTimeout(callback, +wait, ...args)
+  }
+  debounced.cancel = cancel
+  return debounced
+}
 
 const intent = (
   startDebounceEvent,
